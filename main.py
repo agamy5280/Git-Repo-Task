18a6ba9c01950cf404e02e45d48f7cb760e574e8
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2022-10-31 15:06:54 +0200

    Created branch variables and added the requested numbers

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 print("Hello, World!")
-
+txt1 = input("Enter first number:")
+txt2 = input("Enter second number:")
+num1 = int(txt1)
+num2 = int(txt2)
 def printRandomNumbers(num1, num2):
     print(random.randint(num1, num2))
+
+printRandomNumbers(num1, num2)
