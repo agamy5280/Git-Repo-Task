529eff6bb6165484aaa3a3b5dd854e9a14ec23c4
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2022-11-23 22:15:51 +0200

    beautify

--- Full-Page-HTML-CSS/js/cart.js
+++ Full-Page-HTML-CSS/js/cart.js
@@ -1,4 +1,5 @@
 let subtotal = 0;
+
 function addProduct() {
     let productName = document.getElementById("product-name").value;
     let productPrice = document.getElementById("product-price").value;
@@ -11,7 +12,7 @@ function addProduct() {
     ul.classList.add("myClass");
     listProducts.appendChild(ul);
     ul.classList.add("myClass");
-    list.forEach((item)=>{
+    list.forEach((item) => {
         let li = document.createElement("li");
         li.innerText = item;
         li.style.display = "inline";
@@ -21,16 +22,17 @@ function addProduct() {
     document.getElementById("subtotal-cost").innerHTML = subtotal;
     document.getElementById("total-cost").innerHTML = subtotal + 30;
 }
+
 function addCoupon() {
     let couponCode = document.getElementById("coupon").value;
-    if(couponCode == "Sprints" || couponCode == "sprints") {
+    if (couponCode == "Sprints" || couponCode == "sprints") {
         subtotal = document.getElementById("subtotal-cost").innerHTML;
         price = subtotal * 0.25;
         discount = subtotal - price;
         document.getElementById("discount").innerHTML = "-" + discount;
         document.getElementById("total-cost").innerHTML = price + 30;
         document.getElementById("coupon-btn").onclick = null;
-    }else {
+    } else {
         alert("Coupon is invalid!")
     }
 }
