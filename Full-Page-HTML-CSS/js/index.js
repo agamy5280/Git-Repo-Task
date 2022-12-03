2519d2071b60be9418bcf77adec2036c28411a4c
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2022-12-03 23:47:36 +0200

    S_JS_03: OOP: Products with Cart

--- Full-Page-HTML-CSS/js/index.js
+++ Full-Page-HTML-CSS/js/index.js
@@ -1,4 +1,16 @@
-const addSingleProductToCart = (product) => {
+class Product {
+    id;
+    productName;
+    price;
+    constructor(id, productName, price) {
+      this.id = id;
+      this.productName = productName;
+      this.price = price;
+    }
+  }
+class saveProduct {
+  constructor(){}
+  saveToLocalStorage(product) {
     const products = JSON.parse(localStorage.getItem("products") || "[]");
     const oldProductIndex = products.findIndex((x) => x.id === product.id);
     if (oldProductIndex >= 0) {
@@ -7,5 +19,11 @@ const addSingleProductToCart = (product) => {
       products.push({ ...product, quantity: 1 });
     }
     localStorage.setItem("products", JSON.stringify(products));
-  };
-  
+  }
+} 
+const addSingleProductToCart = (product)=>{
+  let newProduct;
+  newProduct = new Product(product.id, product.productName, product.price);
+  let productSave = new saveProduct();
+  productSave.saveToLocalStorage(newProduct);
+}
