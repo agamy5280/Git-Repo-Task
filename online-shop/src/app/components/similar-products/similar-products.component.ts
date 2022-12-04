2c8885281b46642dd58d1a2d8c9cd558e45b6f85
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2022-12-04 21:08:42 +0200

    Similar products fixed bug not showing

--- online-shop/src/app/components/similar-products/similar-products.component.ts
+++ online-shop/src/app/components/similar-products/similar-products.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+declare var $:any
 @Component({
   selector: 'app-similar-products',
   templateUrl: './similar-products.component.html',
   styleUrls: ['./similar-products.component.css']
 })
-export class SimilarProductsComponent {
+export class SimilarProductsComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    $('.related-carousel').owlCarousel({
+      loop: true,
+      margin: 29,
+      nav: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:2
+          },
+          768:{
+              items:3
+          },
+          992:{
+              items:4
+          }
+      }
+  });
+  }
 }
