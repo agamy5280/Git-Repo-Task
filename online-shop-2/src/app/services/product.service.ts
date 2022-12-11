606fdbdc9178b746583ea0ca7617ba2117d8555d
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2022-12-11 18:07:32 +0200

    S_ANG_02: Implement HomePage Component

---
+++ online-shop-2/src/app/services/product.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
+import { Product } from '../interfaces/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  heartClick: number = 0;
+
+  cartProducts: Product[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.heartClick = Number(localStorage.getItem("heatClicked") || "0");
+  }
+
+  getFeaturedProducts(): any {
+    return this.httpClient.get(`${environment.apiUrl}products/getFeatured`);
+  }
+
+  getProducts(): any {
+    return this.httpClient.get(`${environment.apiUrl}products`);
+  }
+
+  getRecentProducts(): any {
+    return this.httpClient.get(`${environment.apiUrl}products/getRecent`);
+  }
+
+  addProduct(product: Product): void {
+    this.cartProducts.push(product);
+  }
+
+  getProductById(id:string){
+    return this.httpClient.get(`${environment.apiUrl}products/${id}`)
+  }
+
+  getProductByCategoryId(id:string){
+    return this.httpClient.get(`${environment.apiUrl}products/getByCategoryId/${id}`);
+  }
+  likeClicked() {
+    this.heartClick += 1;
+    localStorage.setItem("heatClicked", JSON.stringify(this.heartClick));
+  }
+}
