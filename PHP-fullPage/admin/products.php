81e0fdc91a675ec08382ecee3ef8f4a8843411ce
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2023-01-09 16:11:27 +0200

    Updated: products items from db

--- PHP-fullPage/admin/products.php
+++ PHP-fullPage/admin/products.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../layouts/header.php");
-require_once("../php/products.php");
+// require_once("../php/products.php");
+require_once("../php/products_db.php");
+$products = getProducts();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -41,21 +43,25 @@ require_once("../php/products.php");
       
       <th scope="row"><?php echo $index ?></th>
       <td><?= $prod['name'] ?></td>
-      <td><img class="img-fluid w-100" src="<?= '../' . $prod['img'] ?>" alt="<?= $prod['name'] ?>" id="img-products" /></td>
+      <td><img class="img-fluid w-100" src="<?= '../' . $prod['image_url'] ?>" alt="<?= $prod['name'] ?>" id="img-products" /></td>
       <td>$<?= $prod['price'] ?></td>
       <td><?= ( $prod['discount'] * 100 ) ?>%</td>
       <td><?= $prod['rating']?></td>
       <td><?= $prod['rating_count']?></td>
       <?php
-      if ($prod["is_featured"]) {
+      if ($prod["is_featured"] && $prod["is_recent"]) {
+      ?>
+      <td><button type="button" class="btn btn-success" disabled id="add-btn">Yes</button></td>
+      <td><button type="button" class="btn btn-success" disabled id="add-btn">Yes</button></td>
+      <?php
+      }
+      else if ($prod["is_featured"]) {
       ?>
       <td><button type="button" class="btn btn-success" disabled id="add-btn">Yes</button></td>
       <td><button type="button" class="btn btn-danger" disabled id="add-btn">No</button></td>
-      <?php
-      }
-      ?>
-      <?php
-      if ($prod["is_recent"]) {
+    <?php
+    }
+    else if ($prod["is_recent"]) {
       ?>
       <td><button type="button" class="btn btn-danger" disabled id="add-btn">No</button></td>
       <td><button type="button" class="btn btn-success" disabled id="add-btn">Yes</button></td>
@@ -72,7 +78,7 @@ require_once("../php/products.php");
     ?>
   </tbody>
 </table>
-<button type="button" class="btn btn-success" id="add-btn">Add</button>
+<a href="add-products.php"><button type="button" class="btn btn-success" id="add-btn">Add</button></a>
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
---
+++ PHP-fullPage/php/products_db.php
@@ -0,0 +1,13 @@
+<?php
+require_once("../php/dal.php");
+function getProducts($where = '1=1', $limit = '')
+{
+    $query = "SELECT p.*,c.name category_name,s.name size_name,cl.name color_name,r.rating,r.rating_count FROM 
+    products p JOIN categories c ON p.category_id=c.id
+    JOIN sizes s on s.id = p.size_id
+    JOIN colors cl on cl.id = p.color_id
+	LEFT JOIN (SELECT product_id,AVG(rate) rating,COUNT(0) rating_count FROM `ratings` GROUP BY product_id) r ON r.product_id = p.id
+    WHERE $where 
+    $limit";
+    return get_rows($query);
+}
